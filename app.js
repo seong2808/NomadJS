7627c43e4c9bb2799a5a0cb972d00641782fba66
seong2808 <seong2808@naver.com>
2023-03-13 18:34:31 +0900

    second commit

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 // const loginForm = document.getElementById("login-form");
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
+
+const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const userName = loginInput.value;
-    console.log("Hello", userName);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText= `Hello ${userName}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function handleLinkClick(event){
+    event.preventDefault();
+    console.dir(event);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit); 
+link.addEventListener("click", handleLinkClick);
