8592249b93a475f8efc08c5d8f9710e47cef74cd
seong2808 <seong2808@naver.com>
2023-03-13 18:58:50 +0900

    Login commit

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
-// const loginForm = document.getElementById("login-form");
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
-
-const link = document.querySelector("a");
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    const userName = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    greeting.innerText= `Hello ${userName}`;
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick(event){
-    event.preventDefault();
-    console.dir(event);
-}
-
 loginForm.addEventListener("submit", onLoginSubmit); 
-link.addEventListener("click", handleLinkClick);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+console.log(savedUsername);
+
+if(savedUsername === null) {
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit); 
+} else {
+    //show the greetings
+    paintGreetings(savedUsername);
+}
